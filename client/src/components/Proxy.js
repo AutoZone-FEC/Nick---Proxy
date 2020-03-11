619f3f9890e049a7332ffccd9ada7972b1ed94d8
Nicholas Lingrel <nicholas.lingrel@gmail.com>
2020-03-11 17:35:45 -0500

    "Proxy component"

---
+++ client/src/components/Proxy.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Proxy extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+        <div>
+          Proxy is rendering from Proxy
+        </div>
+        )
+    }
+}
+
+export default Proxy
--- proxy.js
+++ proxy.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const axios = require('axios')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
 
 const app = express()
 const port = 8080
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(cors())
+
+app.use(express.static(path.join(__dirname,"../client/dist")));
+
+app.get('/', (req, res) => res.send('Proxy!'))
 
 app.listen(port, () => console.log(`Proxy listening on port ${port}!`))

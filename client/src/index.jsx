abd400e35fa6ed8314a82e52c67a5a2f66ca088e
Nicholas Lingrel <nicholas.lingrel@gmail.com>
2020-03-11 20:18:52 -0500

    "Proxy rendering its own component"

---
+++ client/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Proxy from './components/Proxy'
+// import Reviews from 'http://localhost:8084/server.js'
+
+ReactDOM.render(<Proxy />, document.getElementById('root'))
+// ReactDOM.render(<Reviews />, document.getElementById('reviews'))
